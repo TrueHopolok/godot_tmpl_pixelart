e460ac78afefd8d400bd366a545f172ca8522867
TrueHopolok <vadim.truhanov@inbox.lv>
2026-08-10 20:33:08 +0300

    fixed dialog buttons crashing

--- ui/menus/main_menu/reset_button.gd
+++ ui/menus/main_menu/reset_button.gd
@@ -2,4 +2,4 @@ extends BetterButton
 
 
 func _on_press() -> void:
-	($ResetDialog as Node2D).show()
+	($ResetDialog as ConfirmationDialog).visible = true
--- ui/menus/pause_menu/return_button.gd
+++ ui/menus/pause_menu/return_button.gd
@@ -2,4 +2,4 @@ extends BetterButton
 
 
 func _on_press() -> void:
-	($ReturnDialog as Node2D).show()
+	($ReturnDialog as ConfirmationDialog).visible = true
